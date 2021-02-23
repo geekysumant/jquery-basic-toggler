669c9e8853774768049d82eeda0989840c8646ca
Sumant <sumant@Sumants-MacBook-Air.local>
2021-02-23 22:32:15 +0530

    add all features

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+var check=false;
 
-$9
+$('#div1').click(function(){
+    $('h1').toggleClass("color-white");
+    $('body').toggleClass("bg-color-black");
+    if(!check){
+        $('#circle').css('marginLeft',"100px");
+        check=true;
+    }
+    else{
+        $('#circle').css('marginLeft',"1px");
+        check=false;
+    }
+})
